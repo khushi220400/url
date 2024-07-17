7c23d428ebd63c14b74b2c858370b71fdf08b250
khushi220400 <11e21khushi@gmail.com>
2024-07-17 17:12:54 +0530

    ayush

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express")
+const path=require('path')
+const cookieParser=require('cookie-parser')
+const staticRoute=require('./routes/staticRouter')
+const userRoute=require('./routes/user')
+const { connectToMongoDB } = require("./connect")
+const {checkForAuthentication,
+    restrictTo}=require('./middleware/auth')
+const URL = require('./model/url')
+const urlRoute = require('./routes/url')
+const app = express()
+const PORT = 8001
+
+
+connectToMongoDB('mongodb://localhost:27017/short-url').then(() => console.log("mongodb connected"))
+app.set("view engine","ejs")
+app.set('views',path.resolve("./views"))
+
+
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+app.use(cookieParser())
+app.use(checkForAuthentication)
+app.use("/url",restrictTo(["NORMAL",'ADMIN']),urlRoute)
+app.use("/user",userRoute)
+app.use('/',staticRoute)
+app.get('/url/:shortId', async (req, res) => {
+    const shortId = req.params.shortId;
+    const entry = await URL.findOneAndUpdate({
+        shortId
+    },
+     {
+        $push: {
+            visitHistory: {
+                timestamp: Date.now(),
+            },
+        }
+    })
+    res.redirect(entry.redirectURL)
+
+})
+
+app.listen(PORT, () => console.log(`server started at PORT:${PORT}`))
